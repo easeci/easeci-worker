68ad581ed72b1d33a25e2e043b2f244c3e71cc94
Karol Meksuła <meksula.karol.93@gmail.com>
2022-01-18 23:12:39 +0100

    [worker #0001] Split project and extract 'easeci-worker'

---
+++ src/main/java/io/easeci/worker/connect/HealthCheckController.java
@@ -0,0 +1,16 @@
+package io.easeci.worker.connect;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/api/v1/health-check")
+public class HealthCheckController {
+
+    @Get
+    @Produces(MediaType.APPLICATION_JSON)
+    public HealthCheckResponse healthCheck() {
+        return new HealthCheckResponse("I'm OK");
+    }
+}
