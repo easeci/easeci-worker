8bfd236fd4c8c50feb3786aefd45f9b246c9088a
Karol Meksuła <karol.meksula@fenige.pl>
2022-06-08 23:47:27 +0200

    [core #0060] Implementation of Support for application clustering and worker node management

--- src/main/java/io/easeci/worker/connect/state/ConnectionStateResponse.java
+++ src/main/java/io/easeci/worker/connect/state/ConnectionStateResponse.java
@@ -1,10 +1,17 @@
 package io.easeci.worker.connect.state;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
-public class ConnectionStateResponse extends ConnectionStateRequest {
+@Builder
+@AllArgsConstructor
+public class ConnectionStateResponse {
     private NodeConnectionState nodeConnectionState;
+    private String nodeIp;
+    private String nodePort;
+    private String domainName;
+    private String nodeName;
+    private TransferProtocol transferProtocol;
 }
